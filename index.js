fc69b64223a89ffcf1c6a61f1cf1414d2fc32b51
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-04-14 23:37:40 +0500

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+let matn = String(prompt("string turdagi ma'lumot kriting:"));
+let son = Number(prompt("Number turdagi ma'lumot kriting:"));
+let rost = Boolean(prompt("Boolean turdagi ma'lumot kriting:"));
+
+confirm("Hamma ma'lumotlar to'g'rimi")
+
+alert("Sizning kiritgan " + matn + " so'zi " + typeof matn + " typega tegishl? \n" +
+      "Sizning kiritgan " + son + " soni " + typeof son + " typega tegishli \n" +
+      "Sizning kiritgan " + rost + " so'zi " + typeof rost + " typega tegishli \n")
